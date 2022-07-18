1c31c563a870195f0bef0d1a94f104538c9d2572
Vlad Mahdysh <vlad.mahdysh@apriorit.com>
2022-07-18 16:48:42 +0300

    test

---
+++ Translator_server/handlers.py
@@ -0,0 +1,15 @@
+
+def threading_handler(client_socket):
+    """
+    Takes a client socket and creates a new thread for handling all requests
+    """
+    pass
+
+
+def multiprocessing_handler(client_socket):
+    """
+    Takes a cleint socket and start a new process for handling all client requests
+    """
+    pass
+
+
