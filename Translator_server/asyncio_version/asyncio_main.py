0faa2c78c53ec744f806de011baf5830c90465a0
Vladislav Magdysh <v.magdysh@gmail.com>
2022-08-06 12:32:28 +0300

    Improved asyncio version of server. Fixed several bugs

--- Translator_server/asyncio_version/asyncio_main.py
+++ Translator_server/asyncio_version/asyncio_main.py
@@ -2,7 +2,8 @@ import asyncio, socket
 import logging
 import sys
 from contextlib import closing
-from async_google_trans_new import AsyncTranslator
+from gpytranslate import Translator
+
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,42 +13,51 @@ IP = '127.0.0.1'
 PORT = 5555
 MESSAGE_SIZE = 1024
 
-translator = AsyncTranslator()
+translator = Translator()
 
 
 async def client_processing(client_socket: socket, client_address: socket):
+    loop = asyncio.get_event_loop()
     with closing(client_socket):
         while True:
             data = None
             try:
-                data = client_socket.recv(MESSAGE_SIZE)
+                data = await loop.sock_recv(client_socket, MESSAGE_SIZE)
             except ConnectionResetError:
                 logger.warning("Client dropped the existing connection")
             if not data:
                 break
 
             word_in_english = data.partition(b'\n')[0]
+            if not word_in_english:
+                continue
+
             word_in_english = word_in_english.decode("utf-8")
             logger.info(f"Client {client_address} request: value = {word_in_english}")
             try:
-                answer = await translator.translate(word_in_english, lang_src='en', lang_tgt='uk')
+                answer = await translator.translate(word_in_english, sourcelang="en", targetlang="uk")
+
             except Exception as ex:
                 logger.error(str(ex))
                 continue
 
-            client_socket.sendall(answer.encode("utf-8"))
-            logger.info(f"Response {client_address}:  value = {answer}")
+            client_socket.sendall(answer.text.encode("utf-8"))
+            logger.info(f"Response {client_address}:  value = {answer.text}")
 
 async def run_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.setblocking(False)
     server.bind((IP, PORT))
     server.listen(10)
-    server.setblocking(False)
 
     loop = asyncio.get_event_loop()
     logger.info(f"The server has started ip: {IP} port {PORT}")
     while True:
+        # If I don`t use await loop.sock_accept - I get BlockingIOError
+        # I tried block try/except + pass but server.accept didn`t return control to process client_processing
         client_socket, client_address = await loop.sock_accept(server)
+        logger.info('Connected: {}'.format(client_address))
         client_socket.setblocking(False)
         loop.create_task(client_processing(client_socket, client_address))
 
