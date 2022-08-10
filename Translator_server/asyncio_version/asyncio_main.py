54f57ac2d8e9324af293d0055cac2a68910eddc4
Vladislav Magdysh <v.magdysh@gmail.com>
2022-08-10 20:33:23 +0300

    Created constansts for the hardcoded values

--- Translator_server/asyncio_version/asyncio_main.py
+++ Translator_server/asyncio_version/asyncio_main.py
@@ -9,8 +9,6 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-IP = '127.0.0.1'
-PORT = 5555
 MESSAGE_SIZE = 1024
 
 translator = Translator()
@@ -40,17 +38,26 @@ async def client_processing(client_socket: socket, client_address: socket):
             except Exception as ex:
                 logger.error(str(ex))
                 continue
-
-            client_socket.sendall(answer.text.encode("utf-8"))
+            await loop.sock_sendall(client_socket, answer.text.encode("utf-8"))
             logger.info(f"Response {client_address}:  value = {answer.text}")
 
-async def run_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+PROTOCOL_NUMBER = 0
+REUSE_ADDR_FLAG_VALUE = 1
+LISTEN_CONNECTION_NUMBER = 10
+IP = '127.0.0.1'
+PORT = 5555
+
+
+def _init_server() -> socket:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=PROTOCOL_NUMBER)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, REUSE_ADDR_FLAG_VALUE)
     server.setblocking(False)
     server.bind((IP, PORT))
-    server.listen(10)
+    server.listen(LISTEN_CONNECTION_NUMBER)
+    return server
 
+async def run_server():
+    server = _init_server()
     loop = asyncio.get_event_loop()
     logger.info(f"The server has started ip: {IP} port {PORT}")
     while True:
